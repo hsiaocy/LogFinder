a03d5ca1e634265711f30ce4670b033cbe68b4e0
Allen Hsiao <hsiaocy@fuji.waseda.jp>
2020-09-24 23:57:53 +0800

    remove main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import dev
-
-# dev.build_test_files()
-
-
-res_path = r"C:\Users\ALLENHSIAO\SWE\SS-LIB-PYN-Test-MonitorLog\LogSeeker\search_result\\"
-test_dir = r"C:\Users\ALLENHSIAO\SWE\SS-LIB-PYN-Test-MonitorLog\LogSeeker\test_dir\\"
-dev.findLog('hello', 'world', 'goodbye', folder_path=test_dir, save_path=res_path)
